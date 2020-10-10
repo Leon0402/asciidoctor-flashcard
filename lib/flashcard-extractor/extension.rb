5d378a7bd0fb778c0245fb9c3bb3f85dd4bb6efe
Leon De Andrade <leondeandrade@mailo.com>
2020-10-11 00:41:17 +0200

    Code improvement

--- lib/flashcard-extractor/extension.rb
+++ lib/flashcard-extractor/extension.rb
@@ -5,7 +5,6 @@ class FlashcardExtractor < Extensions::TreeProcessor
   def process document
     return unless document.blocks?
     flashCards = document.find_by(context: :flashcard)
-    document.blocks.clear()
-    document.blocks.concat(flashCards)
+    document.blocks.replace(flashCards)
   end
 end
